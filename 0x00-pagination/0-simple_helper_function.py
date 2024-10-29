26c9ef04a92ae5b412a8e3d79cb906e98724e75a
Dymond <123193636+Ufoma@users.noreply.github.com>
2024-10-29 04:48:31 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,5 +13,5 @@ def index_range(page, page_size):
   """
   start_index = (page - 1) * page_size
   end_index = start_index + page_size
-  return start_index, end_index
+  return (start_index, end_index)
   
